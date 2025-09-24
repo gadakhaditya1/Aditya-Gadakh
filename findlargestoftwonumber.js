fd1701169e5dfc746cf39a1cfafa4d31c2bc3123
unknown <gadakhaditya1@gmail.com>
2025-09-24 19:59:43 +0530

    findlargestoftwonumber

--- findlargestoftwonumber.js
+++ findlargestoftwonumber.js
@@ -1,7 +1,16 @@
- let n1 = 15,
-        n2 = 35;
-      if (n1 > n2) {
-        console.log("largest>", n1);
-      } else {
-        console.log("largest=", n2);
-      }
+//  let n1 = 15,
+//         n2 = 35;
+//       if (n1 > n2) {
+//         console.log("largest>", n1);
+//       } else {
+//         console.log("largest=", n2);
+//       }
+
+
+let a=prompt("enter a number");
+let b=prompt("enter a number");
+if(a<b){
+    console.log(a+ "Smallest");
+}else {
+    console.log(b+ "largest");
+};
